43255aba87ea2e8aad926240ce9a19212cc7c10e
Dmitry Popov <dmpprog@gmail.com>
2020-08-27 15:16:35 +0300

    function for aim-card prototype completed

--- script.js
+++ script.js
@@ -3,28 +3,38 @@
 let aimMass = [];
 
 function tryF () {
+    const formData = new FormData(event.target);
     let obj = {};
-    let aim = document.querySelector('.aim').value;
-    let finalSum= document.querySelector('.final-sum').value;
-    let startSum = document.querySelector('.start-sum').value;
-    let rate = document.querySelector('.rate').value;
+    let aim = formData.get('aim');
+    let finalSum= formData.get('final-sum');
+    let startSum = formData.get('start-sum');
+    let rate = formData.get('rate');
+    let radioM = formData.get('radio-m');
+    let radio = formData.get('radio');
+    let time = formData.get('time');
+    let monthlyPayment = formData.get('monthly-payment');
 
     obj.aim = aim;
     obj['final-sum'] = finalSum;
     obj['start-sum'] = startSum;
     obj.rate = rate;
+    obj['radio-m'] = radioM;
+    obj.radio = radio;
+    obj.time = time;
+    obj['monthly-payment'] = monthlyPayment;
 
     aimMass.push(obj);
+    document.querySelector('#aim').reset();
     console.log(aimMass);
     return aimMass;
 }
 
-document.querySelector('.aim-data button').addEventListener('click', (event) => {
+document.querySelector('.aim-data').addEventListener('submit', (event) => {
     event.preventDefault();
     tryF();
 })
 
-console.log(aimMass);
+
 
 
 
