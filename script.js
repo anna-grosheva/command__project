15e9f74e445157e80a55fd8eb891d5f16c289539
Dmitry Popov <dmpprog@gmail.com>
2020-08-27 14:16:37 +0300

    function for aim-card prototype added

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 'use strict';
 
+let aimMass = [];
+
+function tryF () {
+    let obj = {};
+    let aim = document.querySelector('.aim').value;
+    let finalSum= document.querySelector('.final-sum').value;
+    let startSum = document.querySelector('.start-sum').value;
+    let rate = document.querySelector('.rate').value;
+
+    obj.aim = aim;
+    obj['final-sum'] = finalSum;
+    obj['start-sum'] = startSum;
+    obj.rate = rate;
+
+    aimMass.push(obj);
+    console.log(aimMass);
+    return aimMass;
+}
+
+document.querySelector('.aim-data button').addEventListener('click', (event) => {
+    event.preventDefault();
+    tryF();
+})
+
+console.log(aimMass);
+
+
+
+
+
+
+
+
+
