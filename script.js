b643bd7342b8e84ea3ec6d58dbdede26a06db87e
Dmitry Popov <dmpprog@gmail.com>
2020-08-28 17:49:12 +0300

    JS cleared

--- script.js
+++ script.js
@@ -49,8 +49,6 @@ function createAimUnit () {
 document.querySelector('.aim-data').addEventListener('submit', (event) => {
     event.preventDefault();
     createAimUnit();
-    //
-    // getMonthPayment();
     chooseAction ();
     createAimCard();
     document.querySelector('.aim-data').reset();
@@ -65,46 +63,15 @@ function createAimCard () {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('aim-card');
 
-    // let card = document.querySelector('.aim-card');
-    // cardDiv.innerHTML = card.innerHTML;
+    let card = document.querySelector('.aim-card');
+    cardDiv.innerHTML = card.innerHTML;
 
-    // let removeButton = cardDiv.querySelector('.remove');
-    // removeButton.addEventListener('click', () => {
-    //     cardDiv.remove();
-    // });
+    let removeButton = cardDiv.querySelector('.remove');
+    removeButton.addEventListener('click', () => {
+        cardDiv.remove();
+    });
 
-    const firstInner = document.createElement('div');
-    const headH3 = document.createElement('h3');
-    headH3.classList.add('aim-card__heading');
-    headH3.innerText = aimMass[objectSearch()].aim;
-    const mainContent = document.createElement('p');
-    mainContent.innerText = 'Текст';
-    mainContent.classList.add('aim-card__text');
-    firstInner.append(headH3);
-    firstInner.append(mainContent);
 
-    const secondInner = document.createElement('div');
-    secondInner.classList.add('aim-card__buttons');
-    const editButton = document.createElement('button');
-    editButton.classList.add('aim-card__single-button');
-    editButton.classList.add('edit');
-    editButton.innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
-        '                        <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>\n' +
-        '                        <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>\n' +
-        '                    </svg>';
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('aim-card__single-button');
-    removeButton.classList.add('remove');
-    removeButton.innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
-        '                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>\n' +
-        '                        <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>\n' +
-        '                        <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>\n' +
-        '                    </svg>';
-    secondInner.append(editButton);
-    secondInner.append(removeButton);
-
-    cardDiv.append(firstInner);
-    cardDiv.append(secondInner);
 
     form.parentNode.insertBefore(cardDiv, form.nextSibling);
 }
@@ -154,10 +121,7 @@ function getMonthPayment() {
     console.log(result);
 }
 
-/*
 
-45646545464654
- */
 
 /*
 Функция выбора вычисления
@@ -173,7 +137,6 @@ function chooseAction () {
     }
 }
 
-// (aimMass[0]['radio-m'])
 
 
 // кнопка Добавить
