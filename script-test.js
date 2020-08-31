3ed618c54f13b205daf7d5254c8ec3c7d0286520
Dmitry Popov <dmpprog@gmail.com>
2020-08-31 12:22:20 +0300

    one function app with monthly payments

--- script-test.js
+++ script-test.js
@@ -158,3 +158,205 @@ radioMonthlyPayment.addEventListener('click', () => {
     aimDataPayment.style.display = 'flex';
     aimDataButton.style.display = 'block';
 });
+
+//************************************************
+'use strict';
+
+let form = document.querySelector('form');
+let card = document.querySelector('.aim-card');
+let divAllCards = document.querySelector('.all-cards');
+
+/*
+Функция создания объекта цели из полей формы
+ */
+
+let aimMass = [];
+
+function createAimUnit () {
+    const formData = new FormData(event.target);
+    let obj = {};
+    let aim = formData.get('aim');
+    let finalSum= formData.get('final-sum');
+    let startSum = formData.get('start-sum');
+    let rate = formData.get('rate');
+    let radioM = formData.get('radio-m');
+    let radio = formData.get('radio');
+    let time = formData.get('time');
+    let monthlyPayment = formData.get('monthly-payment');
+
+    obj.aim = aim;
+    obj['final-sum'] = finalSum;
+    obj['start-sum'] = startSum;
+    obj.rate = rate;
+    obj['radio-m'] = radioM;
+    obj.radio = radio;
+    obj.time = time;
+    obj['monthly-payment'] = monthlyPayment;
+
+
+    aimMass.push(obj);
+    // document.querySelector('.aim-data').reset();
+    console.log(aimMass);
+    return aimMass;
+}
+
+
+/*
+Функция прописывания результатов вычислений в карточку цели
+ */
+
+`Для цели`
+
+/*
+Обработчик на отправку данных
+ */
+
+document.querySelector('.aim-data').addEventListener('submit', (event) => {
+    event.preventDefault();
+    createAimUnit();
+    // chooseAction();
+    // createAimCard();
+    render();
+    document.querySelector('.aim-data').reset();
+})
+
+/*
+Функция создания карточки цели
+ */
+
+function render () {
+    divAllCards.innerHTML = '';
+    for (let i = 0; i < aimMass.length; i ++) {
+        let index = i;
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('aim-card');
+        cardDiv.innerHTML = card.innerHTML;
+        cardDiv.querySelector('h3').innerText = aimMass[i].aim;
+
+        getMonthPayment(index);
+
+        let removeButton = cardDiv.querySelector('.remove');
+        removeButton.addEventListener('click', () => {
+            cardDiv.remove();
+        });
+
+        let editButton = cardDiv.querySelector('.edit');
+        editButton.addEventListener('click', () => {
+            cardDiv.classList.add('selected');
+        });
+
+        divAllCards.prepend(cardDiv);
+
+        console.log(divAllCards);
+    }
+}
+
+/*
+Поиск объекта для вычислений и заполнения полей карточки цели
+ */
+
+function objectSearch () {
+    let a = aimMass.length - 1;
+    return a;
+}
+
+
+/*
+Вычисление количества месяцев, необходимых для накопления нужной суммы
+ */
+
+// function getMonthsAmount () {
+//
+//     let result = 0;
+//     let startSum = +aimMass[0]['start-sum'];
+//     let finalSum = +aimMass[0]['final-sum'];
+//     let rate = +aimMass[0].rate;
+//     let monthPay = +aimMass[0]['monthly-payment'];
+//
+//     while (startSum < finalSum) {
+//         startSum = startSum + (startSum * (rate / 12 / 100)) + monthPay;
+//         result += 1;
+//     }
+//     console.log(result);
+// }
+
+/*
+Вычисление ежемесячного платежа
+ */
+
+function getMonthPayment(index) {
+    let result = 0;
+    let startSum = +aimMass[index]['start-sum'];
+    let finalSum = +aimMass[index]['final-sum'];
+    let rate = +aimMass[index].rate;
+    let time = +aimMass[index].time;
+
+    aimMass[index]['monthly-payment'] = (finalSum - (startSum + (startSum * (rate / 12 / 100)) * time)) / time;
+
+    console.log(aimMass[index]['monthly-payment']);
+    console.log(aimMass);
+}
+
+
+
+/*
+Функция выбора вычисления
+ */
+
+function chooseAction (index) {
+    if (aimMass[index].radio === 'on') {
+        console.log(aimMass[index].radio === 'on');
+        getMonthPayment();
+    } else if (aimMass[index]['radio-m'] === 'on') {
+        console.log(aimMass[index]['radio-m'] === 'on');
+        getMonthsAmount();
+    }
+}
+
+
+
+// кнопка Добавить
+
+let createButton = document.querySelector('.create');
+
+form.style.display = 'none';
+card.style.display = 'none';
+
+createButton.addEventListener('click', () => {
+    form.style.display = 'block';
+})
+
+//Выбор radio и сохранить
+
+let aimDataMonths = document.querySelector('.aim-data__months');
+let aimDataPayment = document.querySelector('.aim-data__payment');
+let radioTime = document.querySelector('#radio-time');
+let radioMonthlyPayment = document.querySelector('#radio-monthly-payment');
+let aimDataButton = document.querySelector('.aim-data__button');
+
+aimDataButton.addEventListener('click', () => {
+    form.style.display = 'none';
+})
+
+radioTime.addEventListener('click', () => {
+    aimDataMonths.style.display = 'flex';
+    aimDataPayment.style.display = 'none';
+    aimDataButton.style.display = 'block';
+});
+radioMonthlyPayment.addEventListener('click', () => {
+    aimDataMonths.style.display = 'none';
+    aimDataPayment.style.display = 'flex';
+    aimDataButton.style.display = 'block';
+});
+
+
+function radiocheck1() {
+    if ( this.value === 'radio-t'){
+        console.log('time');
+        getMonthsAmount();
+    } else if (this.value === 'radio-m') {
+        getMonthPayment();
+        console.log('month');
+    }
+}
+
--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function createAimUnit () {
     obj.time = time;
     obj['monthly-payment'] = monthlyPayment;
 
+
     aimMass.push(obj);
     // document.querySelector('.aim-data').reset();
     console.log(aimMass);
@@ -58,8 +59,9 @@ function createAimUnit () {
 document.querySelector('.aim-data').addEventListener('submit', (event) => {
     event.preventDefault();
     createAimUnit();
-    chooseAction();
-    createAimCard();
+    // chooseAction();
+    // createAimCard();
+    render();
     document.querySelector('.aim-data').reset();
 })
 
@@ -67,10 +69,16 @@ document.querySelector('.aim-data').addEventListener('submit', (event) => {
 Функция создания карточки цели
  */
 
-function createAimCard () {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('aim-card');
-    cardDiv.innerHTML = card.innerHTML;
+function render () {
+    divAllCards.innerHTML = '';
+    for (let i = 0; i < aimMass.length; i ++) {
+        // let index = i;
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('aim-card');
+        cardDiv.innerHTML = card.innerHTML;
+        cardDiv.querySelector('h3').innerText = aimMass[i].aim;
+
+        getMonthPayment(i);
 
     divAllCards.prepend(cardDiv);
 
@@ -85,7 +93,7 @@ function createAimCard () {
         editCardData();
     });
 
-    
+
 }
 
 /*
@@ -103,54 +111,58 @@ function editCardData() {
             inputTime.value = aimMass[index].time;
         }
     });
+
+
+        divAllCards.prepend(cardDiv);
+
+        console.log(divAllCards);
 }
-
-
-
+}
 
 /*
 Поиск объекта для вычислений и заполнения полей карточки цели
  */
 
-function objectSearch () {
-    let a = aimMass.length - 1;
-    return a;
-}
+// function objectSearch () {
+//     let a = aimMass.length - 1;
+//     return a;
+// }
 
 
 /*
 Вычисление количества месяцев, необходимых для накопления нужной суммы
  */
 
-function getMonthsAmount () {
-
-    let result = 0;
-    let startSum = +aimMass[0]['start-sum'];
-    let finalSum = +aimMass[0]['final-sum'];
-    let rate = +aimMass[0].rate;
-    let monthPay = +aimMass[0]['monthly-payment'];
-
-    while (startSum < finalSum) {
-        startSum = startSum + (startSum * (rate / 12 / 100)) + monthPay;
-        result += 1;
-    }
-    console.log(result);
-}
+// function getMonthsAmount () {
+//
+//     let result = 0;
+//     let startSum = +aimMass[0]['start-sum'];
+//     let finalSum = +aimMass[0]['final-sum'];
+//     let rate = +aimMass[0].rate;
+//     let monthPay = +aimMass[0]['monthly-payment'];
+//
+//     while (startSum < finalSum) {
+//         startSum = startSum + (startSum * (rate / 12 / 100)) + monthPay;
+//         result += 1;
+//     }
+//     console.log(result);
+// }
 
 /*
 Вычисление ежемесячного платежа
  */
 
-function getMonthPayment() {
+function getMonthPayment(index) {
     let result = 0;
-    let startSum = +aimMass[0]['start-sum'];
-    let finalSum = +aimMass[0]['final-sum'];
-    let rate = +aimMass[0].rate;
-    let time = +aimMass[0].time;
+    let startSum = +aimMass[index]['start-sum'];
+    let finalSum = +aimMass[index]['final-sum'];
+    let rate = +aimMass[index].rate;
+    let time = +aimMass[index].time;
 
-    result = (finalSum - (startSum + (startSum * (rate / 12 / 100)) * time)) / time;
+    aimMass[index]['monthly-payment'] = ((finalSum - (startSum + (startSum * (rate / 12 / 100)) * time)) / time).toFixed(2);
 
-    console.log(result);
+    console.log(aimMass[index]['monthly-payment']);
+    console.log(aimMass);
 }
 
 
@@ -159,15 +171,15 @@ function getMonthPayment() {
 Функция выбора вычисления
  */
 
-function chooseAction () {
-    if (aimMass[0].radio === 'on') {
-        console.log(aimMass[0].radio === 'on');
-        getMonthPayment();
-    } else if (aimMass[0]['radio-m'] === 'on') {
-        console.log(aimMass[0]['radio-m'] === 'on');
-        getMonthsAmount();
-    }
-}
+// function chooseAction (index) {
+//     if (aimMass[index].radio === 'on') {
+//         console.log(aimMass[index].radio === 'on');
+//         getMonthPayment();
+//     } else if (aimMass[index]['radio-m'] === 'on') {
+//         console.log(aimMass[index]['radio-m'] === 'on');
+//         getMonthsAmount();
+//     }
+// }
 
 
 
@@ -190,6 +202,10 @@ let radioTime = document.querySelector('#radio-time');
 let radioMonthlyPayment = document.querySelector('#radio-monthly-payment');
 let aimDataButton = document.querySelector('.aim-data__button');
 
+aimDataButton.addEventListener('click', () => {
+    form.style.display = 'none';
+})
+
 radioTime.addEventListener('click', () => {
     aimDataMonths.style.display = 'flex';
     aimDataPayment.style.display = 'none';
@@ -200,3 +216,14 @@ radioMonthlyPayment.addEventListener('click', () => {
     aimDataPayment.style.display = 'flex';
     aimDataButton.style.display = 'block';
 });
+
+
+// function radiocheck1() {
+//     if ( this.value === 'radio-t'){
+//         console.log('time');
+//         getMonthsAmount();
+//     } else if (this.value === 'radio-m') {
+//         getMonthPayment();
+//         console.log('month');
+//     }
+// }
